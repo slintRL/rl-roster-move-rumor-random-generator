bff26476a295f55aa8c48d7a85706d8fb7e3f7f9
gioele.gentile <gioele.gentile@relatech.com>
2020-07-22 13:17:49 +0200

    first image render try

--- dist/service.js
+++ dist/service.js
@@ -770,5 +770,15 @@ exports.getBlobFromImageURL = getBlobFromImageURL;
 },{}],11:[function(require,module,exports){
 const htmlToImage = require('html-to-image');
 
+var node = document.getElementById('my-node');
 
+htmlToImage.toPng(node)
+.then(function (dataUrl) {
+    var img = new Image();
+    img.src = dataUrl;
+    document.body.appendChild(img);
+})
+.catch(function (error) {
+    console.error('oops, something went wrong!', error);
+});
 },{"html-to-image":9}]},{},[11]);
--- src/service.js
+++ src/service.js
@@ -1,2 +1,13 @@
 const htmlToImage = require('html-to-image');
 
+var node = document.getElementById('my-node');
+
+htmlToImage.toPng(node)
+.then(function (dataUrl) {
+    var img = new Image();
+    img.src = dataUrl;
+    document.body.appendChild(img);
+})
+.catch(function (error) {
+    console.error('oops, something went wrong!', error);
+});
